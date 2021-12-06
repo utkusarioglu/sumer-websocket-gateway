2398ffb1fb9a4bb7b6cf2bf858cd3ae5971a7290
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-06 11:46:59 +0000

    Establish basic kafka -> io
    - Establish kafka -> io. This allows the web-dapp to see ethereum
      events

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
+import http from "http";
+import express from "express";
 import kafkaService from "_services/kafka/kafka.service";
+import { PORT } from "_/__config";
+import eventsService from "_services/events/events.service";
+import { IoService } from "_services/io/io.service";
 
 console.log("kafka is starting");
 kafkaService
   .connectAndSubscribe()
-  .then(() => kafkaService.startListening())
+  .then(() =>
+    kafkaService.startListening({
+      ethereumBlockContent: (data) =>
+        eventsService.ethereumBlockContent.pub(data),
+    })
+  )
   .then(() => console.log("kafka is listening..."));
+
+const app = express();
+const server = http.createServer(app);
+server.listen(PORT, () => console.log(`websocket-gateway started on ${PORT}`));
+const io = new IoService(server);
+io.listen();
